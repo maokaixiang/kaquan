5a20b2bf04449e01c4be9bd8c8382c89a70ce1f7
mkx <1129373068@qq.com>
2019-04-20 09:07:51 +0800

    添加文件

---
+++ components/headnav/headnav.js
@@ -0,0 +1,57 @@
+// components/headnav/headnav.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    xishiduo(){
+      // wx.navigateTo({
+      //   url:""
+      // })
+    },
+    dq(){
+      // wx.navigateTo({
+      //   url:""
+      // })
+    },
+    kfc(){
+      // wx.navigateTo({
+      //   url:""
+      // })
+    },
+    bishengke(){
+      // wx.navigateTo({
+      //   url:""
+      // })
+    },
+    family(){
+      // wx.navigateTo({
+      //   url:""
+      // })
+    },
+    tianfu(){
+      // wx.navigateTo({
+      //   url:""
+      // })
+    },
+    shanlin(){
+      // wx.navigateTo({
+      //   url:""
+      // })
+    }
+  }
+})
